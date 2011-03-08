79275f761f85f47e00b32604ca3ae27742359bdb
Paul van der Walt <paul@denknerd.org>
2011-03-08 16:08:23 +0000

    types seem better now

--- src/CCO/Types.hs
+++ src/CCO/Types.hs
@@ -3,6 +3,7 @@ module CCO.Types where
     import CCO.SourcePos
 
     type Ident = String
+    type Func = (Ident, Ident)
     type Message = String
     data TypeError = Err SourcePos Message
                      deriving Show
